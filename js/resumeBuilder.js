67e60d30435cba2694deb5b4ba5659ffef99572f
Sweta Kumari <sweta191995@gmail.com>
2017-05-13 18:19:27 +0530

    Added the four objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,88 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var bio = {
+    "name" : "Sweta Kumari",
+    "role" : "Front-End Web Developer",
+    "contacts" : {
+        "mobile" : "9234750068",
+        "email" : "sweta191995@gmail.com",
+        "github" : "https://github.com/swetakumari95",
+        "location" : "Bangalore, India"
+    },
+    "welcomeMessage" : "Hey, everyone!",
+    "skills" : ["HTML", "CSS", "JavaScript", "C", "Python"],
+    "biopic" : "https://avatars0.githubusercontent.com/u/25175698?v=3&s=400",
+    "display" : function(){
+
+    }
+};
+
+var education = {
+    "schools" : [{
+        "name" : "Hill Top School",
+        "location" : "Jamshedpur, India",
+        "degree" : "High School",
+        "majors" : ["Computer Science"],
+        "dates" : "1st April,1998 - 31st March,2014",
+        "url" : "http://www.hilltopschooljamshedpur.org/"
+    },
+    {
+        "name" : "RNS Institute Of Technology",
+        "location" : "Bangalore, India",
+        "degree" : "BE",
+        "majors" : ["Computer Science and Engineering"],
+        "dates" : "1st August,2014 - 31st July,2018",
+        "url" : "http://www.rnsit.ac.in/"
+    }],
+    "onlineCourses" : [{
+        "title" : "Front-End Nanodegree",
+        "school" : "Udacity",
+        "dates" : "February,2017 and ongoing",
+        "url" : "https://in.udacity.com/"
+    },{
+        "title" : "HTML&CSS, Jquery, Sass, Ruby, Git",
+        "school" : "Codecademy",
+        "dates" : "2016 onwards",
+        "url" : "https://www.codecademy.com/learn"
+    }],
+    "display" : function(){
+
+    }
+};
+
+var work = {
+    "jobs" : [{
+        "employer" : "Let's Be The Change",
+        "title" : "Front-End Web Developer",
+        "location" : "Bangalore, India",
+        "dates" : "in progress",
+        "description" : "Making a website for the NGO from scratch"
+    }],
+    "display" : function(){
+
+    }
+};
+
+var projects = {
+    "projects" : [{
+        "title" : "Catch em!",
+        "dates" : "November,2016",
+        "description" : "A game to catch as many pigs in one minute, using HTML, CSS, JavaScript and jQuery",
+        "images" : ["images/catchem.png"]
+    },{
+        "title" : "Simple Calculator",
+        "dates" : "December, 2016",
+        "description" : "A simple calculator using HTML, CSS and JavaScript",
+        "images" : ["images/calculator.png"]
+    },{
+        "title" : "Portfolio",
+        "dates" : "March,2017",
+        "description" : "Portfolio website",
+        "images" : ["portfolio.png"]
+    }],
+    "display" : function(){
+
+    }
+};
